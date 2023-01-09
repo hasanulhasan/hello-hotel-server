f8270aa60e5e347a5e40dc6523ebd199917c62cd
Md Hasanul Karim <hasanlink71@yahoo.com>
2023-01-09 14:41:24 +0600

    server ready with MongoDb

--- index.js
+++ index.js
@@ -15,24 +15,42 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try {
     const serviceCollection = client.db('helloHotel').collection('services');
+    const bookingCollection = client.db('helloHotel').collection('bookings');
     app.get('/services', async (req, res) => {
+      const date = req.query.date;
       const query = {};
-      const result = await serviceCollection.find(query).toArray();
-      // const date = req.query.date;
-      // console.log(date);
-      // const query = {};
-      // const options = await servicesCollection.find(query).toArray();
-      // const bookingQuery = { appointmentDate: date }
-      // const alreadyBooked = await bookingCollection.find(bookingQuery).toArray();
-      // options.forEach(option => {
-      //   const optionBooked = alreadyBooked.filter(book => book.treatmentName === option.name);
-      //   const bookSlots = optionBooked.map(book => book.slot);
-      //   const remainingSlots = option.slots.filter(slot => !bookSlots.includes(slot))
-      //   console.log(date, option.name, remainingSlots.length);
-      //   option.slots = remainingSlots;
-      // })
-      res.send(result);
+      const options = await serviceCollection.find(query).toArray();
+      const bookingQuery = { date: date }
+      const alreadyBooked = await bookingCollection.find(bookingQuery).toArray();
+      options.forEach(option => {
+        const optionBooked = alreadyBooked.filter(book => book.name === option.name);
+        const bookRooms = optionBooked.map(book => book.roomNumber);
+        const remainingRooms = option.rooms.filter(room => !bookRooms.includes(room))
+        option.rooms = remainingRooms;
+      })
+      res.send(options);
     });
+
+    app.post('/bookings', async (req, res) => {
+
+      const booking = req.body
+      //if we want to limit to a man to book only one room in one day............
+      // console.log(booking);
+      // const query = {
+      //   date: booking.date,
+      //   name: booking.name,
+      //   // email: booking.email
+      // };
+      // const alreadyBooked = await bookingCollection.find(query).toArray();
+      // if (alreadyBooked.length) {
+      //   const message = `You already have an book on ${booking.date}`
+      //   res.send({ acknowledged: false, message })
+      // }
+      // else {
+      // }
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    })
   }
   finally {
 
